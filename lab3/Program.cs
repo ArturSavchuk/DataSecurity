960ab51bca9c877d35b6c024f35f6470c11b3c1d
Arturich <artursavchuk7@gmail.com>
2020-12-08 15:38:19 +0200

    added simulation of casino player, init lcg generator

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,19 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Numerics;
 using System.Net;
 using System.IO;
 using Newtonsoft.Json.Linq;
 
 namespace lab3
 {
-
-    static class Program
+    public class CasinoPlayer
     {
-        public static long modinv(long a, long modulus)
+        private int id;
+        public CasinoPlayer(int id) { this.id = id; }
+        public void CreateAcc()
+        {
+            WebRequest webRequest = WebRequest.Create(String.Format("http://95.217.177.249/casino/createacc?id={0}", id.ToString()));
+            WebResponse response = webRequest.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    { 
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            response.Close();
+        }
+        public long[] Get3RealNumbers()
+        {
+            long[] states = new long[3];
+            for (int i = 0; i < 3; i++) {
+                WebRequest webRequest = WebRequest.Create(String.Format("http://95.217.177.249/casino/playLcg?id={0}&bet=1&number=1", id));
+                WebResponse response = webRequest.GetResponse();
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string line = "";
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            dynamic test = JObject.Parse(line);
+                            Console.WriteLine(test);
+                            states[i] = (long)test.realNumber;
+                        }
+                    }
+                }
+                response.Close();
+            }
+            return states;
+        }  
+        public void MakeLuckyBet(long realmumber)
+        {
+
+        }
+        
+    }
+
+    public class PRNG_LCG
+    {
+        private long multiplier;
+        private long increment;
+        private long modulus;
+
+        //states for calculating multiplier and increment
+
+        private long seed;
+
+        public PRNG_LCG() { modulus = 4294967296;}
+       
+        public long modinv(long a, long modulus)
         {
             if (modulus == 1) return 0;
             long m0 = modulus;
@@ -27,63 +86,45 @@ namespace lab3
             }
             return x < 0 ? x + m0 : x;
         }
-        public static long crack_multiplier(long[] states, long modulus)
+        public void crack_multiplier(long[] states, long modulus)
         {
             long difference = states[2] - states[1];
             if (difference < 0)
             {
                 difference += modulus;
             }
-            long multiplier = (difference * modinv((states[1] - states[0]), modulus)) % modulus;
-            return multiplier;
+            multiplier = (difference * modinv((states[1] - states[0]), modulus)) % modulus;
 
         }
-        public static long crack_increment(long[] states, long modulus, long multiplier)
+        public void crack_increment(long[] states, long modulus, long multiplier)
         {
-            long increment = (modulus - ((states[0] * multiplier) % modulus) + states[1]) % modulus;
-            return increment;
+            increment = (modulus - ((states[0] * multiplier) % modulus) + states[1]) % modulus;
         }
+        public void init(long[] states)
+        {
+            seed = states[states.Length - 1];
+            crack_multiplier(states, modulus);
+            crack_increment(states, modulus, multiplier);
+        }
+        public long genNext()
+        {
+           
+            seed =  ((multiplier * seed + increment) % modulus);
+            return seed;
+        }
+    }
+    static class Program
+    {
 
         static void Main(string[] args)
         {
             //    //modulus = 2^32 = 4294967296
-            //    long[] states = { 3848296624, 4007236687, 2320102754};
+            long[] states = { 3848296624, 4007236687, 2320102754 };
 
-            //    Console.WriteLine("a = "+ crack_multiplier(states, 4294967296));
-            //    Console.WriteLine("c = "+ crack_increment(states, 4294967296, crack_multiplier(states, 4294967296)));
-
-            //getting first 3 states for cracking
-            long[] states = new long[3];
-            for (int i = 0; i < 3; i++)
-            {
-                WebRequest webRequest = WebRequest.Create("http://95.217.177.249/casino/playLcg?id=1&bet=1&number=1");
-                WebResponse response = webRequest.GetResponse();
-
-                using (Stream stream = response.GetResponseStream())
-                {
-                    using (StreamReader reader = new StreamReader(stream))
-                    {
-                        string line = "";
-                        while ((line = reader.ReadLine()) != null)
-                        { 
-                            dynamic test = JObject.Parse(line);
-                            Console.WriteLine(test);
-                            states[i] = (long)test.realNumber; 
-                        }
-                    }
-                }
-                response.Close();
-            }
-
-            for(int i = 0; i < 3; i++)
-            {
-                Console.WriteLine(states[i]);
-            }
-
-
-
-
-
+            PRNG_LCG pRNG_LCG = new PRNG_LCG();
+            pRNG_LCG.init(states);
+            Console.WriteLine(pRNG_LCG.genNext());
+            
         }
     }
 }
