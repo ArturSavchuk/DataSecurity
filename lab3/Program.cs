7aa170356a24e4124c88b5005391f9080a69d3fc
Arturich <artursavchuk7@gmail.com>
2020-12-08 14:50:52 +0200

    getting realnumbers from casino

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
-
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace lab3
 {
 
-    static class Program 
+    static class Program
     {
         public static long modinv(long a, long modulus)
         {
@@ -25,12 +27,12 @@ namespace lab3
             }
             return x < 0 ? x + m0 : x;
         }
-        public static long crack_multiplier (long[] states, long modulus)
+        public static long crack_multiplier(long[] states, long modulus)
         {
             long difference = states[2] - states[1];
             if (difference < 0)
             {
-                 difference += modulus;
+                difference += modulus;
             }
             long multiplier = (difference * modinv((states[1] - states[0]), modulus)) % modulus;
             return multiplier;
@@ -44,11 +46,44 @@ namespace lab3
 
         static void Main(string[] args)
         {
-            //modulus = 2^32 = 4294967296
-            long[] states = { 3848296624, 4007236687, 2320102754};
+            //    //modulus = 2^32 = 4294967296
+            //    long[] states = { 3848296624, 4007236687, 2320102754};
 
-            Console.WriteLine("a = "+ crack_multiplier(states, 4294967296));
-            Console.WriteLine("c = "+ crack_increment(states, 4294967296, crack_multiplier(states, 4294967296)));
+            //    Console.WriteLine("a = "+ crack_multiplier(states, 4294967296));
+            //    Console.WriteLine("c = "+ crack_increment(states, 4294967296, crack_multiplier(states, 4294967296)));
+
+            //getting first 3 states for cracking
+            long[] states = new long[3];
+            for (int i = 0; i < 3; i++)
+            {
+                WebRequest webRequest = WebRequest.Create("http://95.217.177.249/casino/playLcg?id=1&bet=1&number=1");
+                WebResponse response = webRequest.GetResponse();
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string line = "";
+                        while ((line = reader.ReadLine()) != null)
+                        { 
+                            dynamic test = JObject.Parse(line);
+                            Console.WriteLine(test);
+                            states[i] = (long)test.realNumber; 
+                        }
+                    }
+                }
+                response.Close();
+            }
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(states[i]);
+            }
+
+
+
+
+
         }
     }
 }
